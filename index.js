6a3b4f925e16474e79eae7728560272c448574d6
faridul22 <96929890+faridul22@users.noreply.github.com>
2023-06-12 19:57:33 +0600

    user collection get api added

--- index.js
+++ index.js
@@ -32,6 +32,11 @@ async function run() {
         const selectedClassesCollection = client.db("proDrawing").collection("selectedclasses");
 
         // user collection apis
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result)
+        })
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             const query = { email: user.email };
